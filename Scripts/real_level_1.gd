30abcc2071b480343f786fd1b44c61326600a5b1
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2024-08-14 14:39:31 -0400

    8

--- Scripts/real_level_1.gd
+++ Scripts/real_level_1.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var label = %Label
+@onready var win_panel = %Win
+@onready var lose_sharvil = %Loss
+
 const red = Color(1.0,0.0,0.0,1.0)
 const green = Color(0.0, 1.0, 0.0, 1.0)
 const target_time = 7.5
@@ -12,6 +15,7 @@ func _process(delta):
 func _physics_process(delta):
 	if (stopwatch > target_time):
 		label.set("theme_override_colors/font_color", red)
+		win_panel.visible = true
 	else:
 		label.set("theme_override_colors/font_color", green)
 	label.text = "Your Time: " + str(round(stopwatch)) + " seconds"
