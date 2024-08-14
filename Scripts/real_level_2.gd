ca7bd765397242b6ee04c9e7eeac88c9f73ad174
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2024-08-14 17:35:54 -0400

    frustrating time trying to do music and sfx

--- Scripts/real_level_2.gd
+++ Scripts/real_level_2.gd
@@ -1,21 +1,13 @@
 extends Node2D
 
 @onready var label = %Label
-@onready var result_label = %ResultLabel
-@onready var lose_music
+@onready var win_panel = %Win
+@onready var lose_sharvil = %Loss
 
 const red = Color(1.0,0.0,0.0,1.0)
 const green = Color(0.0, 1.0, 0.0, 1.0)
 const target_time = 7.5
 var stopwatch = 0.0
-
-#func play_lose_sfx():
-	#sfx_player.stream = sound
-	#sfx_player.play()
-
-func _ready():
-	if (get_tree().paused):
-		get_tree().paused = false
 
 func _process(delta):
 	stopwatch += delta
@@ -23,13 +15,7 @@ func _process(delta):
 func _physics_process(delta):
 	if (stopwatch > target_time):
 		label.set("theme_override_colors/font_color", red)
-		result_label.set("theme_override_colors/font_color", red)
-		result_label.text = "Result: Loss"
-		$RealGameMusic.stop()
-
-		get_tree().paused = true
-		await get_tree().create_timer(4).timeout
-		get_tree().reload_current_scene()
+		win_panel.visible = true
 	else:
 		label.set("theme_override_colors/font_color", green)
 	label.text = "Your Time: " + str(round(stopwatch)) + " seconds"
